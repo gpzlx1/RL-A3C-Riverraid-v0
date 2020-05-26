de4c79941e87a024871df09d926f4f2f41135e85
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2020-05-27 05:25:00 +0800

    finish lstm bachward

--- A3C/layers.py
+++ A3C/layers.py
@@ -58,7 +58,34 @@ class LSTMCell(Layer):
             self.bias_ih = None
             self.bias_hh = None
 
+        #中间变量
+        self.pre_inputs = None
+        self.ingate = None
+        self.forgetgate = None
+        self.cellgate = None
+        self.outgate = None
+        self.cy = None
+        self.cx = None
+        self.hy = None
+        self.hx = None
         
+        #梯度
+        self.grad_pre_inputs = None
+        self.grad_ingate = None
+        self.grad_forgetgate = None
+        self.grad_cellgate = None
+        self.grad_outgate = None
+        self.grad_cy = None
+        self.grad_cx = None
+        self.grad_hy = None
+        self.grad_hx = None
+        
+        self.grad_weight_ih = None
+        self.grad_weight_hh = None
+        self.grad_bias_ih = None
+        self.grad_bias_hh = None
+        
+
     def init_weight(self, random=True, loc=0.0, scale=1):
         if random:
             self.weight_ih = torch.Tensor(np.random.normal(loc=0.0, scale=1, size=self.weight_ih.shape))
@@ -81,36 +108,116 @@ class LSTMCell(Layer):
     def load_weights(self):
         pass
 
-    def forward(self, input, hidden):
+    def forward(self, inputs, hidden):
         # https://medium.com/@andre.holzner/lstm-cells-in-pytorch-fab924a78b1c
         hx, cx = hidden
-        gates = F.linear(input, self.weight_ih, self.bias_ih) + F.linear(hx, self.weight_hh, self.bias_hh)
+        self.hx = hx
+        self.cx = cx
+        self.pre_inputs = inputs
+        gates = F.linear(self.pre_inputs, self.weight_ih, self.bias_ih) + F.linear(self.hx, self.weight_hh, self.bias_hh)
         
         ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
 
-        ingate = F.sigmoid(ingate) # i_t 第二个
-        forgetgate = F.sigmoid(forgetgate) # f_t 第一个
-        cellgate = F.tanh(cellgate) # g_t 第三个
-        outgate = F.sigmoid(outgate) # o_t 第四个
+        self.ingate = torch.sigmoid(ingate) # i_t 第二个
+        self.forgetgate = torch.sigmoid(forgetgate) # f_t 第一个
+        self.cellgate = torch.tanh(cellgate) # g_t 第三个
+        self.outgate = torch.sigmoid(outgate) # o_t 第四个
 
-        cy = (forgetgate * cx) + (ingate * cellgate)
-        hy = outgate * F.tanh(cy)
-
-        return hy, cy
+        self.cy = (self.forgetgate * self.cx) + (self.ingate * self.cellgate)
+        self.hy = self.outgate * torch.tanh(self.cy)
+        return self.hy, self.cy
         
 
-    def backward(self):
-        pass
+    def backward(self, top_grad_h, top_grad_c):
+        grad_outgate =  torch.tanh(self.cy) * top_grad_h
+        grad_c = (1 - (torch.tanh(self.cy))**2) * self.outgate * top_grad_h
 
-from envs import create_atari_env
+        grad_forgetgate = self.cx * grad_c
+        grad_ingate = self.cellgate * grad_c
+        grad_cellgate = self.ingate * grad_c
+
+        df_input = self.forgetgate * (1 - self.forgetgate) * grad_forgetgate
+        di_input = self.ingate  * (1 - self.ingate) * grad_ingate
+        dg_input = (1 - (self.cellgate)**2) * grad_cellgate
+        do_input = self.outgate  * (1 - self.outgate) * grad_outgate
+
+        grad_Wif = df_input.t().matmul(self.pre_inputs)
+        grad_Whf = df_input.t().matmul(self.hx)
+        grad_bf = df_input  
+
+        grad_Wii = di_input.t().matmul(self.pre_inputs)
+        grad_Whi = di_input.t().matmul(self.hx)
+        grad_bi =  di_input 
+
+        grad_Wig = dg_input.t().matmul(self.pre_inputs)
+        grad_Whg = dg_input.t().matmul(self.hx)
+        grad_bg = dg_input 
+
+        grad_Wio = do_input.t().matmul(self.pre_inputs)
+        grad_Who = do_input.t().matmul(self.hx)
+        grad_bo = do_input
+
+        self.grad_weight_ih = torch.cat((grad_Wii, grad_Wif, grad_Wig, grad_Wio))
+        self.grad_weight_hh = torch.cat((grad_Whi, grad_Whf, grad_Whg, grad_Who))
+        self.grad_bias_hh = torch.cat((grad_bi, grad_bf, grad_bg, grad_bo),1).squeeze(0)
+        self.grad_bias_ih = self.grad_bias_hh
+
+        bottom_grad_c = self.forgetgate * grad_c
+        
+        param_i, param_f, param_g, param_o = self.weight_hh.chunk(4,0)
+        bottom_grad_h = di_input.matmul(param_i) + df_input.matmul(param_f) +\
+                dg_input.matmul(param_g) + do_input.matmul(param_o)
+
+        return bottom_grad_h, bottom_grad_c
+
+class LSTMTest(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(LSTMTest, self).__init__()
+        self.lstm = torch.nn.LSTMCell(input_size, hidden_size)
+
+    def forward(self, inputs):
+        inputs, (hx, cx) = inputs
+        hx, cx = self.lstm(inputs, (hx, cx))
+        return hx, cx
+
 
 if __name__ == "__main__":
+    
+        
+    cx = torch.randn((1, 256), requires_grad=True)
+    hx = torch.randn((1, 256), requires_grad=True)
+    inputs = torch.randn(1,32*3*3)
+
+
+    #标准model
+    test = LSTMTest(32 * 3 * 3, 256)
+    h1, c1 = test((inputs, (hx,cx)))
+
+    #print(h1)
+    #print(c1)
+    h1.sum().backward()
+    
+
     LSTM = LSTMCell(32 * 3 * 3, 256)
-    cx = torch.zeros(1, 256)
-    hx = torch.zeros(1, 256)
-    env = create_atari_env("Riverraid-v0")
-    state = env.reset()
-    state = torch.Tensor(state)
-    print(state.shape)
+    LSTM.weight_hh = test.lstm.weight_hh#.copy()
+    LSTM.weight_ih = test.lstm.weight_ih
+    LSTM.bias_hh = test.lstm.bias_hh
+    LSTM.bias_ih = test.lstm.bias_ih
+    print(LSTM.weight_hh.shape)
+    print(LSTM.bias_hh.shape)
+    h2,c2 = LSTM.forward(inputs, (hx,cx))
+    #print(h2)
+    #print(c2)
+    bottom_grad_h, bottom_grad_c = LSTM.backward(torch.ones(h1.shape),None)
+    print(sum(sum(abs(test.lstm.weight_hh.grad - LSTM.grad_weight_hh))))
+    print(sum(sum(abs(test.lstm.weight_ih.grad - LSTM.grad_weight_ih))))
+    print(sum(abs(test.lstm.bias_ih.grad - LSTM.grad_bias_ih)))
+    print(sum(abs(test.lstm.bias_hh.grad - LSTM.grad_bias_hh)))
+    print(test.lstm.bias_hh.grad.shape, LSTM.grad_bias_hh.shape)
+    print(test.lstm.weight_hh.grad.shape, LSTM.grad_weight_hh.shape)
+    print(sum(sum(abs(hx.grad - bottom_grad_h))))
+    print(hx.grad.shape, bottom_grad_h.shape)
+    print(sum(sum(abs(cx.grad - bottom_grad_c))))
+    print(cx.grad.shape, bottom_grad_c.shape)
 
 
