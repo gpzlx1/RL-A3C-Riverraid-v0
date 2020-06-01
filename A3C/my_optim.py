98d0844e6e78645396dc75f32b95c81b1331423c
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2020-06-02 00:06:04 +0800

    use add instead of add_

--- A3C/my_optim.py
+++ A3C/my_optim.py
@@ -57,10 +57,10 @@ class SharedAdam(optim.Adam):
                     grad = grad.add(group['weight_decay'], p.data)
 
                 # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg.mul_(beta1).add(1 - beta1, grad)
                 exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
 
-                denom = exp_avg_sq.sqrt().add_(group['eps'])
+                denom = exp_avg_sq.sqrt().add(group['eps'])
 
                 bias_correction1 = 1 - beta1 ** state['step'].item()
                 bias_correction2 = 1 - beta2 ** state['step'].item()
