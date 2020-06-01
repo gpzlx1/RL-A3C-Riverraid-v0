66700436a030e10e4f10d51edc0eee7c840cd297
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2020-06-01 23:36:08 +0800

    my_main init

---
+++ A3C/my_main.py
@@ -0,0 +1,58 @@
+import os 
+
+import torch
+import torch.multiprocessing as mp 
+
+import my_optim
+from envs import create_atari_env
+from my_model import AcotrCritic
+from my_test import test
+from my_train import train
+
+class config(object):
+    def __init__(self):
+        self.lr = 0.0001
+        self.gamma = 0.99
+        self.gae_lambda = 1.00
+        self.entropy_coef = 0.01
+        self.value_loss_coef = 0.01
+        self.max_grad_norm = 50
+        self.seed = 1
+        self.num_processes = 4
+        self.num_steps = 20
+        self.max_step_length = 1000000
+        self.env_name = 'Riverraid-v0'
+        self.model_path = './model/'
+        self.no_shared = False
+
+if __name__ == '__main__':
+    #env
+    args = config()
+    mp.set_start_method("spawn")
+    os.environ['OMP_NUM_THREADS'] = '1'
+    os.environ['CUDA_VISIBLE_DEVICES'] = ""
+
+
+    torch.manual_seed(params.seed)
+    env = create_atari_env(params.env_name)
+    shared_model = AcotrCritic(env.observation_space.shape[0], env.action_space)
+    shared_model.share_memory()
+
+    optimizer = my_optim.SharedAdam(shared_model.parameters(), lr=params.lr)
+    optimizer.share_memory()
+
+    processes = []
+
+    counter = mp.Value('i', 0)
+    lock = mp.Lock()
+
+    p = mp.Process(target=test, args=(params.num_processes, args, shared_model, counter, args.model_path))
+    p.start()
+    processes.append(p)
+
+    for rank in range(0, params.num_processes):
+        p = mp.Process(target=train, args=(rank, args, shared_model, counter, lock, optimizer))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
