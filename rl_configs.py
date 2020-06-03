5e6faaa652addc9c654e8d52f8921c414834ded3
PING GONG <33480480+gpzlx1@users.noreply.github.com>
2020-06-03 17:01:54 +0800

    Update rl_configs.py

--- rl_configs.py
+++ rl_configs.py
@@ -2,18 +2,4 @@ params = {
         'alg_name': 'random_policy',
         'max_iteration': 100,
         'batch_size': 100,
-        'lr' : 0.0001,
-        'gamma' : 0.99,
-        'gae_lambda' : 1.00,
-        'entropy-coef' : 0.01,
-        'value_loss_coef' : 0.01,
-        'max_grad_norm' : 50,
-        'seed' : 1,
-        'num_processes' : 4,
-        'num_steps' : 20,
-        'max_step_length' : 1000000,
-        'env_name' : 'Riverraid-v0',
-        'no_shared' : False
 }
-
-
