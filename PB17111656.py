8a073a959fcb7f4eab1a2e6b6a5003bfa28aa535
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2020-06-03 16:23:34 +0800

    finish evaluate

--- PB17111656.py
+++ PB17111656.py
@@ -1,9 +1,7 @@
-from copy import deepcopy
-from gym.spaces.box import Box
-from gym.spaces import Discrete
-from A3C.my_model import AcotrCritic
 import os
 import torch
+from gym.spaces import Discrete
+from gym.spaces.box import Box
 import torch.nn.functional as F
 from src.alg.RL_alg import RL_alg
 from src.utils.misc_utils import get_params_from_file
@@ -11,39 +9,9 @@ import numpy as np
 import math
 import cv2
 
-def resize(src, new_size):
-    src = src.astype("float32")
-    H, W, C = src.shape
-    new_H, new_W = new_size
-    dst = np.zeros((new_H, new_W, C))
-    ratio_h = H / new_H
-    ratio_w = W / new_W
-    for i in range(new_H):
-        src_y = ratio_h * i
-        y = math.floor(src_y)
-        v = src_y - y
-        if y < 0:
-            y = 0
-            v = 0
-        elif y >= H - 1:
-            y = H - 2
-            v = 1
-
-        for j in range(new_W):
-            src_x = ratio_w * j
-            x = math.floor(src_x)
-            u = src_x - x
-            if x < 0:
-                x = 0
-                u = 0
-            elif x >= W - 1:
-                x = W - 2
-                u = 1
-
-            dst[i][j] = (1 - v) * (1 - u) * src[y][x] + v * (1 - u) * src[y + 1][x] \
-                        + (1 - v) * u * src[y][x + 1] + v * u * src[y + 1][x + 1]
-
-    return dst.astype("uint8")
+import sys
+sys.path.append('src/alg/PB17111656/A3C')
+from my_model import AcotrCritic
 
 def _process_frame42(frame):
     frame = frame[34:34 + 160, :160]
@@ -79,6 +47,7 @@ class NormalizedEnv():
 
         return (observation - unbiased_mean) / (unbiased_std + 1e-8)
 
+
 def find_model(filename):
     list_models = os.listdir(filename)
     x = ''
@@ -103,23 +72,27 @@ class PB17111656(RL_alg):
         self.ac_space = ac_space
         self.state_dim = ob_space.shape[0]
         self.action_dim = ac_space.n
+        self.action_dim = ac_space.n
         self.normalize = NormalizedEnv()
-        self.model = AcotrCritic(self.normalize.observation_space.shape[0],self.ac_space)
-
+        self.model = AcotrCritic(self.state_dim, self.ac_space)
+        self.hx = torch.zeros(1, 256)
+        self.cx = torch.zeros(1, 256)
         #读取model文件夹的内容
-        params = find_model('../model')
-
+        params = find_model('src/alg/PB17111656/model')
         self.model.load_model(params)
 
     def step(self, state):
         state = self.normalize.observation(state)
         state = torch.Tensor(state)
-        cx = torch.zeros(1, 256)
-        hx = torch.zeros(1, 256)
-        _, logit, _ = self.model.forward((state.unsqueeze(0), (hx, cx)))
+        _, logit, (self.hx, self.cx) = self.model.forward((state.unsqueeze(0), (self.hx, self.cx)))
         prob = F.softmax(logit, dim=-1)
         action = prob.max(1, keepdim=True)[1].numpy()
         return action[0,0]
+
+    def reset(self):
+        self.hx = torch.zeros(1, 256)
+        self.cx = torch.zeros(1, 256)
+
 
     def explore(self, obs):
         # currently, we only need to implement explore function
